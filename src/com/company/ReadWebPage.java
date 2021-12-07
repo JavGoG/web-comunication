1f2393864bdc02251e511bd906f5b35f81e5f28f
Javier <javierggjavier@gmail.com>
2021-12-07 11:46:29 +0000

    Primer commit

---
+++ src/com/company/ReadWebPage.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ReadWebPage {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.bankinter.com/banca/nav/banca-particulares"))
+                .GET() // GET is default
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
